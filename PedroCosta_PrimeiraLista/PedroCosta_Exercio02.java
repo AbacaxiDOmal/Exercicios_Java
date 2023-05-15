ab03e6d742735e34e055a1d86d3aa7ccd81861af
Camargo <131812814+AbacaxiDOmal@users.noreply.github.com>
2023-05-15 17:28:57 -0300

    PedroCosta_Exercio02.java

---
+++ PedroCosta_PrimeiraLista/PedroCosta_Exercio02.java
@@ -0,0 +1,46 @@
+/* 
+ * Descrição: Código que soma 3 números.
+ * Autor: Pedro Camargo.
+ * Data:03/05/2023
+ */
+//Scanner importado.
+
+import java.util.Scanner;
+
+//Método iniciado primeiro.
+
+public class PedroCosta_Exercio02 {
+    public static void main(String[] args) {
+        Scanner leitor = new Scanner(System.in);
+
+        // Variáveis declaradas.
+
+        int n1;
+        int n2;
+        int n3;
+        int n4;
+        double res;
+
+        // Início.
+
+        n1 = n2 = n3 = n4 = 0;
+
+        System.out.println("Insira 4 números!");
+
+        // Leitor para ler os números inseridos pelo usuário.
+
+        n1 = leitor.nextInt();
+        n2 = leitor.nextInt();
+        n3 = leitor.nextInt();
+        n4 = leitor.nextInt();
+
+        // Mostra os calculos ao usuário.
+
+        res = Math.pow(n1, 2) + Math.pow(n2, 2) + Math.pow(n3, 2) + Math.pow(n4, 2);
+
+        System.out.print("A soma dos quadrados dos seus números é: " + res);
+
+        // Fim.
+
+    }
+}
