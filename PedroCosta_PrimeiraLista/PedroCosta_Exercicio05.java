57532354ea2e22d4098dcdfab23b6d76f27bfd32
Camargo <131812814+AbacaxiDOmal@users.noreply.github.com>
2023-05-16 16:34:51 -0300

    PedroCosta_Exercicio05.java

---
+++ PedroCosta_PrimeiraLista/PedroCosta_Exercicio05.java
@@ -0,0 +1,39 @@
+/* 
+ * Descrição: Classifica uma pessoa pela idade.
+ * Autor: Pedro Camargo.
+ * Data:15/05/2023
+ */
+//Scanner importado.
+
+import java.util.Scanner;
+
+// Método iniciado primeiro.
+
+public class PedroCosta_Exercicio05.java {
+    public static void main(String[] args) {
+        Scanner leitor = new Scanner(System.in);
+
+        // Inicializando as variáveis.
+
+        int idade;
+
+        // Início.
+
+        System.out.println("Insira sua idade: ");
+
+        // Leitor para ler a "idade" do usuário.
+
+        idade = leitor.nextInt();
+        if (idade >= 66) {
+            System.out.println("Você é idoso!");
+        } else if (idade > 19) {
+            System.out.println("Você é um adulto!");
+        } else if (idade > 12) {
+            System.out.println("Você é adolescente");
+        } else {
+            System.out.println("Você certamente é criança!");
+        }
+
+        // Fim.
+    }
+}
