4eb7222556fce9f67a2c8a684e3962050859dcb3
Camargo <131812814+AbacaxiDOmal@users.noreply.github.com>
2023-05-16 17:40:21 -0300

    PedroCosta_Exercicio01.java

--- PedroCosta_TerceiraLista/PedroCosta_Exercicio01.java
+++ PedroCosta_TerceiraLista/PedroCosta_Exercicio01.java
@@ -1,1 +1,24 @@
+/*
+ * Descrição: Algoritmo que mostra os números de 1 a 1000.
+ * Autor: Pedro Camargo.
+ * Data:16/05/2023
+ */
+// Scanner importado.
+import java.util.Scanner;
 
+// Método iniciado primeiro.
+
+public class ex1_3 {
+    public static void main(String[] args) {
+
+        Scanner leitor = new Scanner(System.in);
+
+        // Início do código utilizando o laço "for" mostrando os números entre 1 e 1000 ao usuário.
+
+        for (int cont = 0; cont <= 1000; cont++) {
+            System.out.println(cont);
+        }
+
+        // Fim.
+    }
+}
