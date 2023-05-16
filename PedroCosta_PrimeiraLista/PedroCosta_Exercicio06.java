4623539d05a314e1d454fb0037cebb551a6900c0
Camargo <131812814+AbacaxiDOmal@users.noreply.github.com>
2023-05-16 17:00:13 -0300

    PedroCosta_Exercicio06.java

---
+++ PedroCosta_PrimeiraLista/PedroCosta_Exercicio06.java
@@ -0,0 +1,35 @@
+/*
+ * Descrição: Calcula o perímetro do raio inserido pelo usuário.
+ * Autor: Pedro Camargo.
+ * Data:15/05/2023
+ */
+// Scanner importado.
+
+import java.util.Scanner;
+
+// Método iniciado primeiro.
+
+public class PedroCosta_Exercicio06 {
+    public static void main(String[] args) {
+
+        Scanner leitor = new Scanner(System.in);
+
+        // Inicializando as variáveis.
+
+        Double raio, result = 0.0;
+
+        // Lê o valor inserido pelo usuário através do leitor.
+
+        System.out.println("Insira um valor para ser o raio de sua circunferência: ");
+
+       raio = leitor.nextDouble();
+
+       // Calcula o perímetro do raio inserido e o mostra para o usuário.
+
+       result = (6.28 * raio);
+
+       System.out.println ("Este é o perímetro do seu raio!\n" + result);
+
+       // Fim.
+    }
+}
