8b0a04ea3918994f3379aeac35261e84fe8b60a4
Camargo <131812814+AbacaxiDOmal@users.noreply.github.com>
2023-05-28 20:19:12 -0300

    PedroCosta_Exercicio10.java

---
+++ PedroCosta_SegundaLista/PedroCosta_Exercicio10.java
@@ -0,0 +1,36 @@
+/* 
+   Algoritmo que avalia se o número é par ou ímpar.
+   Pedro Camargo
+   17/05/2023
+ */
+// Scanner importado.
+
+import java.util.Scanner;
+
+public class PedroCosta_Exercicio10 {
+    public static void main(String[] args){
+
+        Scanner leia = new Scanner(System.in);
+
+        // Declaração das variáveis.
+
+        int n1 = 0;
+       
+        System.out.print("Digite um número: ");
+
+        // Lê o primeiro número inserido pelo usuário.
+
+        n1 = input.nextInt();
+       
+        // Verifica se é par.
+
+        if(n1 % 2 == 0)
+            System.out.print("\n"+n1+" é par");
+        else
+            System.out.print("\n"+n1+" é impar");
+
+    
+    // Fim
+    
+    }
+}
