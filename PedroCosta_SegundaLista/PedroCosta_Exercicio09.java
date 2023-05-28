1bb6d620b61b08dcf2b8f0ad16b0168cfe0a5526
Camargo <131812814+AbacaxiDOmal@users.noreply.github.com>
2023-05-28 20:13:13 -0300

    PedroCosta_Exercicio09.java

---
+++ PedroCosta_SegundaLista/PedroCosta_Exercicio09.java
@@ -0,0 +1,43 @@
+/* 
+   Algoritmo que mostra se um dos numeros é maior ou se são iguais.
+   Pedro Camargo
+   17/05/2023
+ */
+// Scanner importado.
+
+import java.util.Scanner;
+
+public class PedroCosta_Exercicio09 {
+    public static void main(String[] args){
+
+        Scanner leia = new Scanner(System.in);
+
+        // Declaração das variáveis.
+
+        int n1, n2;
+
+        // Início.
+
+        n1 = n2 = 0;
+
+        Scanner leitor = new Scanner(System.in);
+
+        System.out.println("Digite o primeiro número: ");
+        n1 = leitor.nextInt();
+
+        System.out.println("Digite o segundo número: ");
+        n2 = leitor.nextInt();
+
+        // Decisão encadeada:
+
+        if(n1>n2){
+            System.out.println("O primeiro é maior");
+        }else if(n1<n2){
+            System.out.println("O segundo é maior");
+        }else{
+            System.out.println("Os números são iguais");
+        }
+
+        // Fim.
+    }
+}
