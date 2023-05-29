c5b633a5f5f9bff622bbf042b223f2d39ff53484
Camargo <131812814+AbacaxiDOmal@users.noreply.github.com>
2023-05-29 16:59:27 -0300

    PedroCosta_Exercicio01.java

--- PedroCosta_TerceiraLista/PedroCosta_Exercicio01.java
+++ PedroCosta_TerceiraLista/PedroCosta_Exercicio01.java
@@ -3,15 +3,10 @@
  * Autor: Pedro Camargo.
  * Data:16/05/2023
  */
-// Scanner importado.
-import java.util.Scanner;
-
 // Método iniciado primeiro.
 
 public class PedroCosta_Exercicio01 {
     public static void main(String[] args) {
-
-        Scanner leitor = new Scanner(System.in);
 
         // Início do código utilizando o laço "for" mostrando os números entre 1 e 1000 ao usuário.
 
