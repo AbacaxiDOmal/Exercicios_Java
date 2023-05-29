6b968883efa96414a36320b0b67bca04fbed156f
Camargo <131812814+AbacaxiDOmal@users.noreply.github.com>
2023-05-29 15:52:02 -0300

    PedroCosta_Exercicio02.java

--- PedroCosta_TerceiraLista/PedroCosta_Exercicio02.java
+++ PedroCosta_TerceiraLista/PedroCosta_Exercicio02.java
@@ -10,7 +10,7 @@ import java.util.Scanner;
 public class PedroCosta_Exercicio02 {
     public static void main(String[] args){
 
-        Scanner leia = new Scanner(System.in);
+        Scanner input = new Scanner(System.in);
 
         // Declaração das variáveis.
 
