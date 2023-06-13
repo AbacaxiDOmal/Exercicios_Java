e1f426a80599c140d8a6e5a9542a0e9c46f30726
Camargo <131812814+AbacaxiDOmal@users.noreply.github.com>
2023-06-13 18:24:16 -0300

    PedroCosta_Exercicio04.java

---
+++ PedroCosta_QuintaLista/PedroCosta_Exercicio04.java
@@ -0,0 +1,75 @@
+/* 
+ * Classe qualquer que tem 10 métodos e um construtor e 10 atributos.
+ * Pedro Camargo.
+ * 10/06/2023
+ */
+
+ public class QualquerCoisa {
+    int atributo1;
+    String atributo2;
+    double atributo3;
+    boolean atributo4;
+    char atributo5;
+    int atributo6;
+    String atributo7;
+    boolean atributo8;
+    double atributo9;
+    char atributo10;
+
+    public QualquerCoisa (int atributo1, String atributo2, double atributo3, boolean atributo4, char atributo5, int atributo6,
+            String atributo7, boolean atributo8, double atributo9, char atributo10) {
+        this.atributo1 = atributo1;
+        this.atributo2 = atributo2;
+        this.atributo3 = atributo3;
+        this.atributo4 = atributo4;
+        this.atributo5 = atributo5;
+        this.atributo6 = atributo6;
+        this.atributo7 = atributo7;
+        this.atributo8 = atributo8;
+        this.atributo9 = atributo9;
+        this.atributo10 = atributo10;
+    }
+
+    public void metodo1() {
+        // Implementação do método 1
+    }
+
+    public void metodo2() {
+        // Implementação do método 2
+    }
+
+    public void metodo3() {
+        // Implementação do método 3
+    }
+
+    public void metodo4() {
+        // Implementação do método 4
+    }
+
+    public void metodo5() {
+        // Implementação do método 5
+    }
+
+    public void metodo6() {
+        // Implementação do método 6
+    }
+
+    public void metodo7() {
+        // Implementação do método 7
+    }
+
+    public void metodo8() {
+        // Implementação do método 8
+    }
+
+    public void metodo9() {
+        // Implementação do método 9
+    }
+
+    public void metodo10() {
+        // Implementação do método 10
+    }
+
+}
+
+// fim.
