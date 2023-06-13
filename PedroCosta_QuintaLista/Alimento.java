34baa8d339b34c10554752eb3b2b48da085e6895
Camargo <131812814+AbacaxiDOmal@users.noreply.github.com>
2023-06-13 18:16:52 -0300

    Alimento.java

---
+++ PedroCosta_QuintaLista/Alimento.java
@@ -0,0 +1,24 @@
+/* 
+ * Classe Alimento com 10 atributos!
+ * Pedro Camargo.
+ * 10/06/2023
+ */
+
+ public class Alimento {
+
+    // atributos atribuídos a Classe "Alimento".
+ 
+    String nome;
+    String tipo;
+    String marca;
+    String origem;
+    int valor;
+    int validade;
+    int saude;
+    boolean vegano;
+    boolean transgênico;
+    boolean saudável;
+ 
+ }
+ 
+ // fim.
