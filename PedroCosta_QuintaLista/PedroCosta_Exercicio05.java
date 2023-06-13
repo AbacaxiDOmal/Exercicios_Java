7c1736ceefd6519f0c3475b867d33687ee6f5669
Camargo <131812814+AbacaxiDOmal@users.noreply.github.com>
2023-06-13 18:25:32 -0300

    PedroCosta_Exercicio05.java

---
+++ PedroCosta_QuintaLista/PedroCosta_Exercicio05.java
@@ -0,0 +1,25 @@
+/* 
+ * Classe "vovoh" que possui um método construtor.
+ * Pedro Camargo.
+ * 10/06/2023
+ */
+
+ public class Vovoh {
+
+    // atributos.
+    String raça, classe;
+    int pontosDEvida;
+    int lvl;
+
+    // métodos.
+    Vovoh(String raça, String classe, int pontosDEvida, int lvl){  
+        this.raça = raça;
+        this.classe = classe;
+        this.pontosDEvida = pontosDEvida;
+        this.lvl = lvl;
+
+    }
+  
+}
+
+// fim.
